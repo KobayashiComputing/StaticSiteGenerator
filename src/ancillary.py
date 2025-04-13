5778dccbd9d4893f0b2c2cd3f2fd5f8468003f97
KobayashiComputing <andy@kobayashicomputing.com>
2025-04-13 12:48:41 -0400

    Chapter 4, Lesson 2 'Heading', 'Code', and 'Paragraph' (the default) are done.

--- src/ancillary.py
+++ src/ancillary.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 def markdown_to_blocks(markdown):
     block_list = []
     blocks = markdown.split('\n\n')
@@ -6,3 +9,32 @@ def markdown_to_blocks(markdown):
         if len(block) > 0:
             block_list.append(block)
     return block_list
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block):
+    block_type = BlockType.PARAGRAPH
+
+    # It's a heading if it begins with from 1 to 6 '#' characters...
+    if block[0] == "#":
+        ndx = block.index(" ")
+        if ndx > 0 and ndx < 7 and "###### "[:ndx] == block[:ndx]:
+            block_type = BlockType.HEADING
+    
+    # It's a code block if it both starts and ends with "```"
+    elif block[:3] == "```" and block[-3:] == "```":
+        block_type = BlockType.CODE
+
+    # Anything else should be a plain paragraph...
+    else:
+        block_type = BlockType.PARAGRAPH
+
+    return block_type
+
--- src/test_ancillary.py
+++ src/test_ancillary.py
@@ -67,6 +67,41 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_block_type_code(self):
+        block = "```\nfirst line of code\nsecond line of code\nthird line of code\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+    
+    def test_block_type_heading_1(self):
+        block = "# Heading 1"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_block_type_heading_2(self):
+        block = "## Heading 2"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_block_type_heading_3(self):
+        block = "### Heading 3"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_block_type_heading_4(self):
+        block = "#### Heading 4"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_block_type_heading_5(self):
+        block = "##### Heading 5"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_block_type_heading_6(self):
+        block = "###### Heading 6"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_block_type_heading_7_not(self):
+        block = "####### Heading 7 (Ha! That doesn't exist!)"
+        self.assertNotEqual(block_to_block_type(block), BlockType.HEADING)
+    
+        
+
+
 
 
 if __name__ == "__main__":

ad757bb9f37e6c64db778caab6526967dff95d69
KobayashiComputing <andy@kobayashicomputing.com>
2025-04-22 15:31:02 -0400

    Chapter 5, Lesson 4 complete.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,17 @@ from htmlnode import *
 from extractors import *
 from markdown import *
 
+def get_file_list(source):
+     filelist = []
+     entries = os.listdir(source)
+     for entry in entries:
+         path = os.path.join(source, entry)
+         if os.path.isdir(path):
+             filelist.extend(get_file_list(path))
+         if os.path.isfile(path):
+             filelist.append(path)
+     return filelist
+
 def do_copy(source, destination):
     entries = os.listdir(source)
     for entry in entries:
@@ -44,8 +55,9 @@ def generate_page(from_path, template_path, dest_path):
         print(f"Cannot find template file '{template_path}'")
         return ""
     if not os.path.exists(os.path.dirname(dest_path)):
-        print(f"Destination directory '{os.path.dirname(dest_path)}' not found...")
-        return ""
+        # os.mkdir(os.path.dirname(dest_path))
+        create_dir_path(os.path.dirname(dest_path))
+        # print(f"Destination directory '{os.path.dirname(dest_path)}' not found...")
     
     with open(from_path) as f:
         md = f.read()
@@ -63,9 +75,40 @@ def generate_page(from_path, template_path, dest_path):
 
     return page_title
 
+def create_dir_path(path):
+    parts = path.split('/')
+    path2 = ""
+    for part in parts:
+        if not part == '.':
+            path2 += part
+            if not os.path.exists(path2):
+                os.mkdir(path2)
+            path2 += '/'
+
+        pass
+    pass
+    return True
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    files = get_file_list(dir_path_content)
+    for file in files:
+        dfile = file.replace("./content", "./public", 1)
+        dfile = dfile.replace(".md", ".html", 1)
+        generate_page(file, template_path, dfile)
+        pass
+    pass
+    return True
+
 def main():
+    # Welcome message...
     print("Welcome to the Static Site Generator!")
+
+    # Clean out and populate the destination directory...
     result = clean_and_copy("./static", "./public")
-    result = generate_page("./content/index.md", "./template.html", "./public/index.html")
+
+    # Generate the page(s)...
+    result = generate_pages_recursive("./content", "./template.html", "./public")
+    # result = generate_page("./content/index.md", "./template.html", "./public/index.html")
+    
     print(f"Finished creating HTML page for '{result}'!")
 main()
